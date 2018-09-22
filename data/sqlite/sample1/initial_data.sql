95ba02fae9434dc8669dc78303293c1b2fec1e92
teru_kusu <teru.kusu@gmail.com>
2018-09-22 23:25:29 +0900

    sample1用のSQLite DBを移動

---
+++ data/sqlite/sample1/initial_data.sql
@@ -0,0 +1,2 @@
+DELETE FROM sample_person;
+INSERT INTO sample_person (id, name, age, created_at, updated_at) VALUES (1,'山田 太郎',35, 1536960887, 1536961887),(2,'渡辺 花子',18, 1536970887, 1536971887),(3,'カルロス マルチネス',3, 1536980887, 1536980897);
